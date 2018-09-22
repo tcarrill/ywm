bbca316fa4ad2f5c3b8ddee896fb60d050e3de40
tcarrill <thomas.carrill@gmail.com>
2018-09-22 15:27:59 -0400

    Added doubly linked list

--- test.c
+++ test.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "ylist.h"
 
 typedef struct MenuItem {
 	int id;
 	char *label;
 	char *command;
 } MenuItem;
+
+MenuItem * menuItem_new(char *label, char *command, int index) 
+{
+ 	MenuItem *menuItem = malloc(sizeof(MenuItem));
+	menuItem->id = index;
+	menuItem->label = malloc(sizeof(char) * strlen(label));
+	menuItem->command = malloc(sizeof(char) * strlen(command));
+	strncpy(menuItem->label, label, strlen(label));
+	strncpy(menuItem->command, command, strlen(command));
+	
+	return menuItem;
+}
 
 int main() 
 {
@@ -22,16 +36,36 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 	
+	YList *list = ylist_new();
+	
 	int index = 0;
 	while ((read = getline(&line, &len, fp)) != -1) {
-		printf("%s", line);
+		size_t ln = strlen(line) - 1;
+		if (line[ln] == '\n') {
+		    line[ln] = '\0';
+		}
+		char *label = strtok(line, "|");
+		char *command = strtok(NULL, "|");
+		
+	 	MenuItem *menuItem = menuItem_new(label, command, index);
+		ylist_add_tail(list, menuItem);
+		
 		index++;
 	}
 	
 	fclose(fp);	
 	if (line) {
-		printf("free(line)\n");
 		free(line);
 	}
+	
+	printf("Menu Items: %d\n", list->length);
+	YNode *curr = list->head;
+	while (curr != NULL) {
+		MenuItem *menuItem = (MenuItem*)curr->data;
+		printf("%d: %s (%s)\n", menuItem->id, menuItem->label, menuItem->command);
+		curr = curr->next;
+	}
+	
+	ylist_destroy(list);
 	exit(EXIT_SUCCESS);
 }
